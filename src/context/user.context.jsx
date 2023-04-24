d6330b2c46d10af47b374dfad023974c2554483c
asdfqweasd <229463581@qq.com>
2023-04-24 20:20:41 +1000

    start redux

--- src/context/user.context.jsx
+++ src/context/user.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState, useEffect, useReducer } from "react";
 import {
   onAuthStateChangedListener,
   createUserDocumentFromAuth,
@@ -10,9 +10,27 @@ export const UserContext = createContext({
   setCurrentUser: () => null,
 });
 
+export const USER_ACTION_TYPES = {
+  SET_CURRENT_USER: "SET_CURRENT_USER",
+};
+const INITIAL_STATE = {
+  currentUser: null,
+};
+const userReducer = (state, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case USER_ACTION_TYPES.SET_CURRENT_USER:
+      return { ...state, currentUser: payload };
+    default:
+      throw new Error(`Unhandled type ${type} in userReducer`);
+  }
+};
 export const UserProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null);
-  const value = { currentUser, setCurrentUser };
+  const [{ currentUser }, dispatch] = useReducer(userReducer, INITIAL_STATE);
+
+  const setCurrentUser = (user) =>
+    dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, currentUser: user });
   useEffect(() => {
     const unsubscribe = onAuthStateChangedListener((user) => {
       if (user) {
@@ -23,6 +41,9 @@ export const UserProvider = ({ children }) => {
 
     return unsubscribe;
   }, []);
+  const value = {
+    currentUser,
+  };
   // the unsubscribe will cancel the onAuthStateChangedListener
   return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
 };
